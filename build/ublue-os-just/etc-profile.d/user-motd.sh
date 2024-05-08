25b8ac618de39e1adfe381ab3d8497c9d690db78
Brian Beatty <brian@27megahertz.com>
2024-05-08 07:49:30 -0500

    feat: toggle-user-motd

--- build/ublue-os-just/etc-profile.d/user-motd.sh
+++ build/ublue-os-just/etc-profile.d/user-motd.sh
@@ -1,5 +1,5 @@
 if test -d "$HOME"; then
-  if test ! -e "$HOME"/.config/no-show-user-motd; then
+  if test -e "$HOME"/.config/show-user-motd; then
     if test -x "/usr/libexec/ublue-motd"; then
       /usr/libexec/ublue-motd
     elif test -s "/etc/user-motd"; then
